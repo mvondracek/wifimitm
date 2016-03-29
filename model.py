7e13f54d5a0ef5bf2edd8c118bc69fa3571a0c68
Vondracek Martin <xvondr20@stud.fit.vutbr.cz>
2016-03-29 04:35:38 +0000

    WirelessAccessPoint: cracked PSK as file

--- model.py
+++ model.py
@@ -100,17 +100,24 @@ class WirelessStation(object):
 
 class WirelessAccessPoint(object):
     def __str__(self, *args, **kwargs):  # TODO (xvondr20) just for debugging
-        return 'WirelessAccessPoint(' + ', '.join([
-            self.bssid,
+        s = 'WirelessAccessPoint(' + ', '.join([
+            self.essid,
+            self.bssid
+        ])
+
+        if self.is_cracked():
+            s += ', PSK(0x' + self.cracked_psk + ', "' + bytes.fromhex(self.cracked_psk).decode('ascii') + '"), '
+
+        s += ', '.join([
             self.power,
             self.channel,
             self.encryption,
             self.cipher,
             self.authentication,
             str(self.wps),
-            self.essid,
             self.iv_sum
         ]) + ')'
+        return s
 
     def __init__(self, bssid, power, channel, encryption, cipher, authentication, wps, essid, iv_sum):
         self.bssid = bssid
@@ -124,6 +131,48 @@ class WirelessAccessPoint(object):
         self.iv_sum = iv_sum
 
         self.associated_stations = list()
+
+    @property
+    def dir_path(self):
+        """
+        Get path to directory which should be dedicated for files related to this network.
+        :return: str
+        """
+        return os.path.join(os.getcwd(), 'networks', self.essid)  # TODO (xvondr20) what is essid is not available?
+
+    def make_dir(self):
+        """
+        Make directory to store files related to this network, if does not exist already.
+        """
+        os.makedirs(self.dir_path, exist_ok=True)
+
+    @property
+    def cracked_psk_path(self):
+        """
+        Get path to location where cracked PSK should be located after successful crack.
+        Path to PSK is returned even if the file does not exists (have not been successfully cracked yet).
+        :return: str
+        """
+        return os.path.join(self.dir_path, self.encryption + '_PSK.hex')
+
+    def is_cracked(self):
+        """
+        Decide whether the network have been successfully cracked and therefore a PSK is available.
+        :return: bool
+        """
+        return os.path.isfile(self.cracked_psk_path)
+
+    @property
+    def cracked_psk(self):
+        """
+        Get hexadecimal cracked PSK if available. If the network have not been cracked yet, therefore PSK is not
+        available, returns None.
+        :return: str|None
+        """
+        if self.is_cracked():
+            with open(self.cracked_psk_path, 'r') as f:
+                psk_hex = f.read()
+                return psk_hex
 
     def add_associated_station(self, station):
         station.associated_ap = self
